494f110975d36b2ceb8134dfb1133744ec2c3327
Recep Cankaya <recepcnky@outlook.com>
2024-06-28 10:02:05 +0300

    feat: adding sign in with google

--- android/app/src/main/java/com/anonymous/LadderItApp/MainActivity.kt
+++ android/app/src/main/java/com/anonymous/LadderItApp/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.anonymous.LadderItApp
+package com.ladderit
 
 import android.os.Build
 import android.os.Bundle
--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,31 +1,17 @@
 import { createClient } from "@supabase/supabase-js";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from "@env";
-const SUPABASE_ANON_KEY = PUBLIC_SUPABASE_ANON_KEY;
-const SUPABASE_URL = PUBLIC_SUPABASE_URL;
-console.log("supabase", SUPABASE_URL, SUPABASE_ANON_KEY);
-const supabase = createClient<DB>(SUPABASE_URL, SUPABASE_ANON_KEY, {
-  auth: {
-    storage: AsyncStorage,
-    autoRefreshToken: true,
-    persistSession: true,
-    detectSessionInUrl: false,
-  },
-});
 
-export function createSupabaseClient(accessToken: string) {
-  return createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
-    global: {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
+const supabase = createClient<DB>(
+  process.env.LOCAL_SUPABASE_URL!,
+  process.env.LOCAL_SUPABASE_ANON_KEY!,
+  {
+    auth: {
+      storage: AsyncStorage,
+      autoRefreshToken: true,
+      persistSession: true,
+      detectSessionInUrl: false,
     },
-  });
-}
-
-export const secretSupabase = createClient(
-  "https://gittjeqpqcmmbterylkd.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdpdHRqZXFwcWNtbWJ0ZXJ5bGtkIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcwOTIwNDM2NCwiZXhwIjoyMDI0NzgwMzY0fQ.Y5Zp48dzrtSWzatGtTptbYP-fbvhwqTfQHjmBVNRTSg"
+  }
 );
 
 export default supabase;
